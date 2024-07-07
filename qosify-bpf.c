f6b503a0c4aab90644579f43e65529352bb40bbc
Dengfeng Liu <liudf0716@gmail.com>
2024-07-07 16:40:23 +0800

    feat: add bpf_printk function for debug
    Signed-off-by: Dengfeng Liu <liudf0716@gmail.com>

--- qosify-bpf.c
+++ qosify-bpf.c
@@ -28,6 +28,13 @@
 
 #define EWMA_SHIFT		12
 
+#define bpf_printk(fmt, ...)					\
+({								\
+	char ____fmt[] = fmt;					\
+	bpf_trace_printk(____fmt, sizeof(____fmt),		\
+			 ##__VA_ARGS__);			\
+})
+
 const volatile static uint32_t module_flags = 0;
 
 struct flow_bucket {
